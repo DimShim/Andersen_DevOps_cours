20fe6ad6bb40d4f851c5e6f7485b9019b9a89597
Dima <50591032+DimShim@users.noreply.github.com>
2021-09-13 22:11:08 +0200

    Update fish.sh

--- netstat_script/fish.sh
+++ netstat_script/fish.sh
@@ -1,1 +1,30 @@
+#! /bin/bash
 
+
+# define "help" message
+help=$'Show the names of organizations with which the PROCESS has
+established a connection.'
+
+
+# there is only one argument
+  if [[ $1 == '-h' || $1 == '--help' ]]; then
+    # the only argument is 'help'
+    echo "$help"
+    exit 0
+  fi
+
+
+# final output
+separator_line="+-------------------------------------------------------+"
+echo $separator_line
+printf "%-55s | \n" \
+  "|     Organization name with established connection"
+echo $separator_line
+
+
+# script form task
+ss -tunap | awk '/firefox/ {print $6}' | cut -d: -f1 | 
+sort | uniq -c | sort | tail -n5 | grep -oP '(\d+\.){3}\d+' | 
+while read IP ; do whois $IP | awk -F':' '/^Organization/ {print $2}' ; done
+
+echo $separator_line
